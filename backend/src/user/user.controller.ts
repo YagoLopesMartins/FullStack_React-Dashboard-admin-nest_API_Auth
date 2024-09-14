112bc8636058b425a846766ad11571eb42263195
Yago <ylm@icomp.ufam.edu.br>
2024-09-13 20:40:35 -0400

    chore: Setup Swagger to API documentation

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -12,16 +12,22 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from '@prisma/client';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post()
+  @ApiOperation({ summary: 'Criar um novo usuário' })
+  @ApiResponse({ status: 201, description: 'Usuário criado com sucesso.' })
   create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos os usuários' })
+  @ApiResponse({ status: 200, description: 'Usuários retornados com sucesso.' })
   findAll(
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 10,
@@ -31,11 +37,15 @@ export class UserController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obter um usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário retornado com sucesso.' })
   findOne(@Param('id') id: string): Promise<User | null> {
     return this.userService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Atualizar um usuário' })
+  @ApiResponse({ status: 200, description: 'Usuário atualizado com sucesso.' })
   update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -44,6 +54,8 @@ export class UserController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Deletar um usuário' })
+  @ApiResponse({ status: 204, description: 'Usuário deletado com sucesso.' })
   delete(@Param('id') id: string): Promise<User> {
     return this.userService.delete(id);
   }
