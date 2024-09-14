41348313cf05caffc70a67a9332f0a4f743d50c5
Yago <ylm@icomp.ufam.edu.br>
2024-09-14 01:29:57 -0400

    chore: Add hemelt(security) and enable cors, headers, authorizations to frontend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Response } from 'express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,22 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // Habilitando CORS
+  app.enableCors({
+    origin: '*', // http://localhost:5173
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true, // Habilita cookies
+  });
+
+  app.use((req, res: Response, next) => {
+    res.header('Content-Type', 'application/json');
+    res.header('Cache-Control', 'no-store');
+    next();
+  });
+
+  app.use(helmet());
 
   await app.listen(3000);
 }
