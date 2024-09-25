a090f262786e0a5b2289d7c34aaf12ea402debb0
Yago <ylm@icomp.ufam.edu.br>
2024-09-25 02:52:38 -0400

    feat(frontend): Add behavior from authenticate in Login page with redirect Loading page

--- frontend/src/pages/Loading.tsx
+++ frontend/src/pages/Loading.tsx
@@ -1,14 +1,33 @@
 // src/pages/Loading.tsx
-import React from 'react';
-import { Progress } from "@/components/ui/progress";
-import logo2 from '@/assets/icons/Group 48687@2x.png' // shadcn/ui Loader component
+import React, { useEffect } from 'react'
+import { Progress } from '@/components/ui/progress'
+import logo2 from '@/assets/icons/Group 48687@2x.png'
+import { useNavigate } from 'react-router-dom'
+import { Toaster } from '@/components/ui/toaster.tsx'
+import { useToast } from '@/hooks/use-toast.ts'
 
 const Loading: React.FC = () => {
+    const { toast } = useToast()
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        toast({
+            title: 'Usuário logado com sucesso!',
+            variant: 'success'
+        })
+        setTimeout(() => {
+            navigate('/home')
+        }, 3000) // Simula o tempo de espera de 3 segundos
+    }, [navigate])
+
     return (
         <>
+            <Toaster />
+            <div className="flex justify-center items-center h-screen">
+                <div className="progress w-1/2 bg-blue-500">Loading...</div>
+            </div>
             <div className="flex items-center justify-center h-screen bg-neutral-100">
-
-                {/* Loading Spinner */}
                 <div className="flex flex-col items-center">
                     <div className="hidden lg:flex items-center justify-center w-1/3 p-8">
                         <img src={logo2} alt="Logo" className="max-w-full h-auto" />
@@ -17,8 +36,7 @@ const Loading: React.FC = () => {
                 </div>
             </div>
         </>
+    )
+}
 
-    );
-};
-
-export default Loading;
+export default Loading
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,129 +1,157 @@
-// src/pages/Login.tsx
-import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
-import logo2 from "./../assets/icons/Group 48687@2x.png";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card'
+import logo2 from './../assets/icons/Group 48687@2x.png'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { useToast } from '@/hooks/use-toast.ts'
+import { Toaster } from '@/components/ui/toaster.tsx'
+
+const loginSchema = z.object({
+    email: z.string().email('Email inválido'),
+    password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres')
+})
+
+type LoginFormInputs = z.infer<typeof loginSchema>
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
+    const { toast } = useToast()
 
-  const [emailField, setEmailField] = useState("");
-  const [passwordField, setPasswordField] = useState("");
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [PasswordError, setPasswordError] = useState("");
-  const [EmailError, setEmailError] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("foi");
-    if (senha === "") {
-      document.getElementById("password")?.classList.add("border-b-red-800");
-      setPasswordError("Campo Obrigatorio");
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm()
+
+    const navigate = useNavigate()
+
+    const onSubmit = async (data) => {
+        try {
+            const response = await axios.post('http://localhost:3000/login', {
+                email: data.email,
+                password: data.password
+            })
+            console.log('Autenticação bem-sucedida:', response.data)
+
+            const { access_token, user } = response.data
+
+            localStorage.setItem('access_token', response.data.access_token)
+            localStorage.setItem('user', JSON.stringify(user))
+            toast({
+                title: 'Usuário logado com sucesso!',
+                variant: 'success'
+            })
+            navigate('/loading')
+        } catch (error) {
+            toast({
+                title: 'Usuário/Senha inválido(a)',
+                variant: 'destructive'
+            })
+            console.error('Erro ao autenticar:', error.response?.data || error.message)
+        }
     }
-    if (email === "") {
-      document.getElementById("email")?.classList.add("border-b-red-800");
-      setEmailError("Campo Obrigatorio");
-    } else {
-      console.log();
-    }
-  };
 
-  const handleEnterButton = () => {
-    navigate("/home");
-  };
-
-  return (
-    <div
-      className="relative flex items-center justify-center h-screen"
-      style={{ backgroundColor: "rgb(34, 45, 89)" }}
-    >
-      <div className="hidden lg:flex items-center justify-center w-1/3 p-8">
-        <img src={logo2} alt="Logo" className="max-w-full h-auto" />
-      </div>
-
-      <div
-        style={{ height: "30rem" }}
-        className="flex flex-col justify-center w-full max-w-xl p-8"
-      >
-        <Card className="h-full">
-          <CardHeader className="p-4">
-            <CardTitle
-              style={{ color: "#0290A4" }}
-              className="pt-3 px-5 text-left text-5xl font-bold"
+    return (
+        <>
+            <Toaster />
+            <div
+                className="relative flex items-center justify-center h-screen"
+                style={{ backgroundColor: 'rgb(34, 45, 89)' }} // Background color in RGB
             >
-              Bem-vindo!
-            </CardTitle>
-          </CardHeader>
-          <CardHeader className="py-3 pt-9">
-            <CardTitle className="text-left text-1xl px-5 font-semibold">
-              Entre com sua conta
-            </CardTitle>
-          </CardHeader>
+                <div className="hidden lg:flex items-center justify-center w-1/3 p-8">
+                    <img src={logo2} alt="Logo" className="max-w-full h-auto" />
+                </div>
 
-          <CardContent>
-            <div className="space-y-4 px-5">
-              <div>
-                <Label htmlFor="email"></Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="E-mail ou Nº Matricula"
-                  value={emailField}
-                  onChange={(e) => setEmailField(e.target.value)}
-                />
-                {EmailError && (
-                  <p className="text-red-500 text-sm">{EmailError}</p>
-                )}
-              </div>
-              <div>
-                <Label htmlFor="password"></Label>
-                <Input
-                  id="password"
-                  type="password"
-                  placeholder="Senha"
-                  value={passwordField}
-                  onChange={(e) => setPasswordField(e.target.value)}
-                />
-                {PasswordError && (
-                  <p className="text-red-500 text-sm">{PasswordError}</p>
-                )}
-              </div>
+                {/* Right Side - Login Form */}
+                <div style={{ height: '30rem' }} className="flex flex-col justify-center w-full max-w-xl p-8">
+                    <Card className="h-full">
+                        <CardHeader className="p-4">
+                            <CardTitle style={{ color: '#0290A4' }} className="pt-3 px-5 text-left text-5xl font-bold">
+                                Bem-vindo!
+                            </CardTitle>
+                        </CardHeader>
+                        <CardHeader className="py-3 pt-9">
+                            <CardTitle className="text-left text-1xl px-5 font-semibold">Entre com sua conta</CardTitle>
+                        </CardHeader>
+
+                        <CardContent>
+                            {/* Email Field */}
+                            <div className="space-y-4 px-5">
+                                <div>
+                                    <Label
+                                        htmlFor="email"
+                                        className={`block text-sm font-medium mb-2 ${
+                                            errors.email ? 'text-red-500' : 'text-gray-700'
+                                        }`}
+                                    ></Label>
+                                    <Input
+                                        className={`block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-200 ${
+                                            errors.email
+                                                ? 'border-b-red-800 border-2 placeholder-red-500 text-red-500'
+                                                : 'border-green-500'
+                                        }`}
+                                        type="email"
+                                        placeholder="E-mail ou Nº Matricula"
+                                        {...register('email', { required: true })}
+                                    />
+                                    {errors?.email?.type === 'required' && (
+                                        <p className="text-red-500 text-sm mt-1">Campo obrigatório.</p>
+                                    )}
+                                </div>
+
+                                {/* Password Field */}
+                                <div>
+                                    <Label htmlFor="password"></Label>
+                                    <Input
+                                        className={`block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-200 ${
+                                            errors.email
+                                                ? 'border-b-red-800 border-2 placeholder-red-500 text-red-500'
+                                                : 'border-green-500'
+                                        }`}
+                                        type="password"
+                                        placeholder="Senha"
+                                        {...register('password', { required: true, minLength: 7 })}
+                                    />
+                                    {errors?.password?.type === 'required' && (
+                                        <p className="text-red-500 text-sm mt-1">Campo obrigatório.</p>
+                                    )}
+
+                                    {errors?.password?.type === 'minLength' && (
+                                        <p className="error-message">Password needs to have at least 7 characters.</p>
+                                    )}
+                                </div>
+                            </div>
+                        </CardContent>
+
+                        <CardFooter className="flex flex-col space-y-4" style={{ paddingLeft: '3rem' }}>
+                            {/* Login Button */}
+                            <Button
+                                className="w-full p-5 text-1xl"
+                                onClick={() => handleSubmit(onSubmit)()}
+                                style={{ backgroundColor: '#0290A4' }}
+                            >
+                                Entrar
+                            </Button>
+
+                            {/* Forgot Password */}
+                            <a
+                                href="/forgot"
+                                className="text-sm text-center text-blue-600 hover:underline mt-3 font-bold"
+                                style={{ color: '#0290A4' }}
+                            >
+                                Esqueci minha senha
+                            </a>
+                        </CardFooter>
+                    </Card>
+                </div>
             </div>
-          </CardContent>
+        </>
+    )
+}
 
-          <CardFooter
-            className="flex flex-col space-y-4"
-            style={{ paddingLeft: "3rem" }}
-          >
-            <Button
-              className="w-full p-5 text-1xl"
-              onClick={handleEnterButton}
-              style={{ backgroundColor: "#0290A4" }}
-            >
-              Entrar
-            </Button>
-
-            <a
-              href="/reset"
-              className="text-sm text-center text-blue-600 hover:underline mt-3 font-bold"
-              style={{ color: "#0290A4" }}
-            >
-              Esqueci minha senha
-            </a>
-          </CardFooter>
-        </Card>
-      </div>
-    </div>
-  );
-};
-
-export default Login;
+export default Login
